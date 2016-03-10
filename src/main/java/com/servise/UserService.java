2ba9a45edd0814d1763f307c68004dba747ea8c7
Andrii <Krasav4k1@gmail.com>
2016-03-10 20:36:19 +0300

    commit

---
+++ src/main/java/com/servise/UserService.java
@@ -0,0 +1,25 @@
+package com.servise;
+
+import com.entity.User;
+import com.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    public void addUser(String lastName, String firstName, String password, String emailUser){
+        User user = new User();
+        user.setLastName(lastName);
+        user.setFirstName(firstName);
+        user.setPassword(password);
+        user.setEmail(emailUser);
+        userRepository.save(user);
+        System.out.println(user.getFirstName());
+    }
+
+}
